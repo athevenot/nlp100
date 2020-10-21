15fd1d158803ca22b1afd197b3c5dcdd99dd8f24
Arthur Thévenot <athevenot@outlook.fr>
2020-10-21 13:47:35 +0200

    chap01

--- chap01/src/main.rs
+++ chap01/src/main.rs
@@ -1,6 +1,13 @@
-#![allow(unused)]
+/// chap01 reverse
+pub fn reverse(str: &str) -> String {
+    str.chars().rev().collect()
+}
 
-fn main() {
-    println!("Hello, world!");
-    println!("Hello SSH!");
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_chap01_00_reverse() {
+        assert_eq!("Hello", reverse("olleH"));
+    }
 }
